dd5502dc6d9406a270a6426858eaa8ad7827c628
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 21:23:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ function currentLine(line){
   console.log("The line is currently: " + lineNamesandNumbers)
   return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
+
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
