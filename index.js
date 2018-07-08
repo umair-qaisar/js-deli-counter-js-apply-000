eb97480595e659f346daf205aaa4232b2b8bbcaa
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 21:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(line) {
     console.log("There is nobody waiting to be served!")
     return "There is nobody waiting to be served!"
   } else {
-    console.log("Currently serving " + line.shift());
+    //console.log("Currently serving " + line.shift());
     return "Currently serving " + line.shift();
   }
 }
